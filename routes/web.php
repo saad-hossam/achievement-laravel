79ac23263e8721932964d8f99ce8b48d7a8fb740
salim-eslam <salimeslam55@gmail.com>
2025-05-27 14:09:44 +0300

    route update1

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,8 @@ Route::group(
     function () {
         Route::get('/',  [HomeController::class, 'index'])->name('home');
         // Route::get('/about',  [HomeController::class, 'about'])->name('about');
+        Route::get('/services',  [HomeController::class, 'services'])->name('services');
+
     }
 );
 
